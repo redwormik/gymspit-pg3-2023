365887f890b52fa279ed10d1a1a8b9062d36881c
redwormik <redwormik@gmail.com>
2023-10-09 08:55:15 +0200

    Add Lecture1 Mad Lips solution

---
+++ Lecture1MadLips/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Please enter a number: ");
+
+if (!int.TryParse(Console.ReadLine(), out int level)) {
+	Console.WriteLine("That is not a number!");
+	Environment.Exit(1);
+}
+
+Console.Write("Please enter a species: ");
+string species = Console.ReadLine();
+
+Console.Write("Please enter a class: ");
+string className = Console.ReadLine();
+
+Console.Write("Please enter an action: ");
+string action = Console.ReadLine();
+
+Console.Write("Please enter a weapon: ");
+string weapon = Console.ReadLine();
+
+Console.Write("Please enter an adjective: ");
+string adjective = Console.ReadLine();
+
+Console.Write("Please enter a noun: ");
+string noun = Console.ReadLine();
+
+Console.WriteLine(
+	"A level {0} {1} {2} {3}s their {4} at the {5} {6}.",
+	level,
+	species,
+	className,
+	action,
+	weapon,
+	adjective,
+	noun
+);
