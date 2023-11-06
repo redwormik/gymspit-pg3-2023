cbbe218ef738d54e2436b7268f2f05f4c419a2db
redwormik <redwormik@gmail.com>
2023-11-06 08:46:06 +0100

    Add Lecture8 (first part)

---
+++ Lecture8/Program.cs
@@ -0,0 +1,66 @@
+﻿static bool AddValue(string value, string[] data, int count)
+{
+	if (count >= data.Length) {
+		Console.WriteLine("I'm afraid I can't do that.");
+		return false;
+	}
+
+	data[count] = value;
+	return true;
+}
+
+static bool RemoveValue(string[] data, int index, int count)
+{
+	if (index < 0 || index >= count){
+		Console.WriteLine("I'm afraid I can't do that.");
+		return false;
+	}
+
+	for (int i = index; i < count - 1; i += 1) {
+		data[i] = data[i + 1];
+	}
+	data[count - 1] = "";
+	return true;
+}
+
+static void AddUser(string username, string[] users, ref int userCount)
+{
+	int index = Array.IndexOf(users, username);
+	if (index >= 0) {
+		Console.WriteLine("User already exists.");
+		return;
+	}
+
+	if (AddValue(username, users, userCount)) {
+		userCount += 1;
+	}
+}
+
+static void RemoveUser(string username, string[] users, ref int userCount)
+{
+	int index = Array.IndexOf(users, username);
+	if (index < 0) {
+		Console.WriteLine("User does not exist.");
+		return;
+	}
+
+	if (index >= 0 && RemoveValue(users, index, userCount)) {
+		userCount -= 1;
+	}
+}
+
+
+int MAX_USERS = 100;
+int MAX_POSTS = MAX_USERS * 100;
+int MAX_FOLLOWS = MAX_USERS * (MAX_USERS + 1) / 2;
+
+string[] users = new string[MAX_USERS];
+int userCount = 0;
+
+string[] posts = new string[MAX_POSTS];
+string[] postAuthors = new string[MAX_POSTS];
+int postCount = 0;
+
+string[] followers = new string[MAX_FOLLOWS];
+string[] followees = new string[MAX_FOLLOWS];
+int followCount = 0;
