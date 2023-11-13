6ee02e5656bb58d080dd2f2cf8bb4a2e45c1f0f5
redwormik <redwormik@gmail.com>
2023-11-13 09:40:25 +0100

    Add Lecture8 method signatures

--- Lecture8/Program.cs
+++ Lecture8/Program.cs
@@ -23,6 +23,7 @@ static bool RemoveValue(string[] data, int index, int count)
 	return true;
 }
 
+
 static void AddUser(string username, string[] users, ref int userCount)
 {
 	int index = Array.IndexOf(users, username);
@@ -50,6 +51,49 @@ static void RemoveUser(string username, string[] users, ref int userCount)
 }
 
 
+static void AddPost(string post, string author, string[] posts, string[] postAuthors, ref int postCount)
+{
+	// TODO
+}
+
+static string[] GetUserPosts(string user, string[] posts, string[] postAuthors, int postCount)
+{
+	// TODO
+	return new string[] { };
+}
+
+
+static void AddFollow(string follower, string followee, string[] followers, string[] followees, ref int followCount)
+{
+	// TODO
+}
+
+static void RemoveFollow(string follower, string followee, string[] followers, string[] followees, ref int followCount)
+{
+	// TODO
+}
+
+static string[] GetUserFollows(string user, string[] followers, string[] followees, int followCount)
+{
+	// TODO
+	return new string[] { };
+}
+
+static string[] GetUserFollowers(string user, string[] followers, string[] followees, int followCount)
+{
+	// TODO
+	return new string[] { };
+}
+
+
+// Bonus
+static string[] GetUserTimeline(string user, string[] posts, string[] postAuthors, int postCount, string[] followers, string[] followees, int followCount)
+{
+	// TODO
+	return new string[] { };
+}
+
+
 int MAX_USERS = 100;
 int MAX_POSTS = MAX_USERS * 100;
 int MAX_FOLLOWS = MAX_USERS * (MAX_USERS + 1) / 2;
@@ -64,3 +108,5 @@ int postCount = 0;
 string[] followers = new string[MAX_FOLLOWS];
 string[] followees = new string[MAX_FOLLOWS];
 int followCount = 0;
+
+AddUser("wormik", users, ref userCount);
