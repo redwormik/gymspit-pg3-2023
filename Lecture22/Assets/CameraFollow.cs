daedc361b124eac55899a2cd6dbea4e1278a9956
redwormik <redwormik@gmail.com>
2024-04-08 08:41:35 +0200

    Add Lecture22

---
+++ Lecture22/Assets/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+	public Camera followingCamera;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		followingCamera.transform.position = new Vector3(
+			gameObject.transform.position.x,
+			gameObject.transform.position.y,
+			followingCamera.transform.position.z
+		);
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		followingCamera.transform.position = new Vector3(
+			gameObject.transform.position.x,
+			gameObject.transform.position.y,
+			followingCamera.transform.position.z
+		);
+	}
+}
---
+++ Lecture22/Assets/Controls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+public class Controls : MonoBehaviour
+{
+	public Rigidbody2D rigidBody;
+
+	public float speed;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");	
+
+		// rigidBody.position += new Vector2(moveHorizontal, moveVertical) * speed * Time.deltaTime;
+		// rigidBody.velocity += new Vector2(moveHorizontal, moveVertical) * speed * Time.deltaTime;
+		rigidBody.AddForce(new Vector2(moveHorizontal, moveVertical) * speed * Time.deltaTime);
+	}
+}
